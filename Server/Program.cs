2b75b877e077ebd379bc674cc4b3338974926b5b
rdogan7106 <rdogan7106@gmail.com>
2024-04-12 18:09:00 +0200

    last changing

--- Server/Program.cs
+++ Server/Program.cs
@@ -2,7 +2,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using MySql.Data.MySqlClient;
 using Server;
-string connectionString = "server=localhost;uid=root;pwd=mypassword;database=AuctionDatabase;port=3306";
+string connectionString = "server=localhost;uid=root;pwd=Rd0671rd..;database=AuctionDatabase2;port=3306";
 
 var builder = WebApplication.CreateBuilder(args);
 State state = new(connectionString);
